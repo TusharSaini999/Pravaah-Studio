6525e1130f6a2e4e43d584844718abe89da3ff98
Tusharsaini999 <tusharsaini.in@gmail.com>
2026-01-16 10:52:28 +0530

    Add the Server Config and Setup the Untils

---
+++ Backend/src/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { SIZE_LIMIT, URL_LIMIT } from '../constants.js';
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_URL,
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.use(
+  express.json({
+    limit: SIZE_LIMIT,
+  })
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: URL_LIMIT,
+    parameterLimit: 1000,
+  })
+);
+
+app.use(express.static('public'));
+
+app.use(cookieParser());
+
+export { app };
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,18 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
-
+import { app } from './app.js';
 import connectDB from './db/connectDB.js';
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log('Server Start to Post:', `${process.env.PORT || 8000}`);
+    });
+    app.on('error', (err) => {
+      console.error('Server Error:', err.message);
+      process.exit(1);
+    });
+  })
+  .catch((err) => {
+    console.log('MongoDB Connection Fail so App not Listen', err.message);
+  });
